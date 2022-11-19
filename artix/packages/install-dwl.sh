669cb31eeaf9d9002942a018d13912669c353435
krypek <115574014+krypciak@users.noreply.github.com>
2022-11-19 20:58:36 +0100

    Build somebar at dwl install

--- artix/packages/install-dwl.sh
+++ artix/packages/install-dwl.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 function install_dwl() {
-    echo ''
+    echo 'meson ninja'
 }
 
 function configure_dwl() {
     cd $USER_HOME/.config/dwl/dwl-dotfiles/
     make
+    
+    cd $USER_HOME/.config/dwl/somebar/
+    meson build
+    cd $USER_HOME/.config/dwl/somebar/build
+    ninja
 }
