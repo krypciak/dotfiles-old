e9f6aa521dad3e34ef16aa0108b48605e1db60f7
krypek <115574014+krypciak@users.noreply.github.com>
2022-11-24 21:53:33 +0100

    Add 'alsa-utils'

--- artix/packages/install-audio.sh
+++ artix/packages/install-audio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function install_audio() {
-    echo 'pulseaudio cmus-git playerctl pulseaudio-alsa'
+    echo 'pulseaudio cmus-git playerctl pulseaudio-alsa alsa-utils'
 }
 function configure_audio() {
     pip install cmus-notify
