75d10adddffe25cc6eda28d2759da41adab00134
krypek <115574014+krypciak@users.noreply.github.com>
2022-11-25 16:12:31 +0100

    Add screenshot tools

--- artix/packages/install-wayland.sh
+++ artix/packages/install-wayland.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 function install_wayland() {
-    echo 'wlroots wayland-protocols wayland xorg-xwayland qt6-wayland gammastep inotify-tools fuzzel cliphist swaylock swww'
+    echo 'wlroots wayland-protocols wayland xorg-xwayland qt6-wayland gammastep inotify-tools fuzzel cliphist swaylock swww slurp grim'
 }
