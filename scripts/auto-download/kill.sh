a7d2315f578c2cdeaf4b68f4cfbe252505141391
krypek <115574014+krypciak@users.noreply.github.com>
2022-12-30 18:47:18 +0100

    Add video auto download script

---
+++ scripts/auto-download/kill.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+pkill yt-dlp
+pkill rsstail
---
+++ scripts/auto-download/start_rss.sh
@@ -0,0 +1,48 @@
+export DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+sh $DIR/kill.sh
+
+TEMP_DIR="$DIR/temp"
+VIDEOS_DIR="$DIR/videos"
+
+
+source $DIR/subs.sh
+
+mkdir -p $TEMP_DIR
+mkdir -p $VIDEOS_DIR
+
+
+YT_DLP_ARGS="--sponsorblock-remove all --embed-thumbnail --add-metadata --embed-metadata --embed-chapters --all-subs --embed-subs --audio-quality 0 --rm-cache-dir -R infinite --retry-sleep 60 -S quality,ext:mp4,filesize --download-archive downloaded.txt --no-post-overwrites --restrict-filenames --ignore-errors"
+
+
+function listen_rss() {
+    rsstail -l -N -n 1 -i 300 -P -u "$1" | while read url; do
+
+        # If there is less than 2 GiB space left
+        while [[ "$(df $VIDEOS_DIR | awk 'NR==2{print $4}')" -le 2097152 ]]; do
+            # If no files left to delete, exit
+            if [[ "$(ls $VIDEOS_DIR | wc -l)" -eq 0 ]]; then
+                echo -e "\e[32mLess than 2GiB on $VIDEOS_DIR, exiting\e[0m"
+                exit 1
+            fi
+            # Remove the last video
+            rm -f "$(LC_CTYPE=C ls -t $VIDEOS_DIR | tail -1)"
+        done
+
+        if [[ "$url" == http* ]]; then 
+            echo
+            echo -e "\e[32m${url}\e[0m"
+            yt-dlp $YT_DLP_ARGS -o "%(uploader)s - %(title)s.%(ext)s" -P "$VIDEOS_DIR" -P "temp:$TEMP_DIR" "$url"
+            echo -e "\e[32mDownload Done\e[0m"
+        fi
+    done
+}
+
+i=0
+for feed in ${CHANNEL_FEEDS[@]}; do
+    listen_rss "${INSTANCES[$i]}/feed/channel/$feed" &
+
+    i=$((i+1))
+    if [[ ${#INSTANCES[@]} -eq $i ]]; then
+        i=0
+    fi
+done
