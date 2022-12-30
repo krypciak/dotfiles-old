c492c4df96994759121929ada8ad8fa688a8b305
krypek <115574014+krypciak@users.noreply.github.com>
2022-12-30 22:22:05 +0100

    Update start_rss.sh

--- scripts/auto-download/start_rss.sh
+++ scripts/auto-download/start_rss.sh
@@ -11,11 +11,11 @@ mkdir -p $TEMP_DIR
 mkdir -p $VIDEOS_DIR
 
 
-YT_DLP_ARGS="--sponsorblock-remove all --embed-thumbnail --add-metadata --embed-metadata --embed-chapters --all-subs --embed-subs --audio-quality 0 -R infinite --retry-sleep 900 -S quality,ext:mp4,filesize --download-archive downloaded.txt --no-post-overwrites --restrict-filenames --ignore-errors --newline"
+YT_DLP_ARGS="--sponsorblock-remove all --embed-thumbnail --add-metadata --embed-metadata --embed-chapters --all-subs --embed-subs --audio-quality 0 -R infinite --retry-sleep 900 -S quality,ext:mp4,filesize --download-archive downloaded.txt --no-post-overwrites --ignore-errors --newline"
 
 
 function listen_rss() {
-    rsstail -z -l -N -n 1 -i 900 -P -u "$1" | while read url; do
+    rsstail -z -l -N -n 1 -i 300 -P -u "$1" | while read url; do
 
         # If there is less than 2 GiB space left
         while [[ "$(df $VIDEOS_DIR | awk 'NR==2{print $4}')" -le 2097152 ]]; do
@@ -30,7 +30,7 @@ function listen_rss() {
 
         if [[ "$url" == http* ]]; then 
             echo -e "\e[32m${url}\e[0m"
-            yt-dlp $YT_DLP_ARGS -o "%(uploader)s - %(title)s.%(ext)s" -P "$VIDEOS_DIR" -P "temp:$TEMP_DIR" "$url"
+            yt-dlp $YT_DLP_ARGS -o "%(channel)s - %(title)s.%(ext)s" -P "$VIDEOS_DIR" -P "temp:$TEMP_DIR" "$url"
             echo -e "\e[32mDownload Done\e[0m"
         fi
     done
@@ -45,3 +45,8 @@ for feed in ${CHANNEL_FEEDS[@]}; do
         i=0
     fi
 done
+
+for feed in ${ODYSEE_FEEDS[@]}; do
+    listen_rss "https://odysee.com/$/rss/$feed" &
+done
+
