15233d4ec10c5d5fe7017bc2e9849fcfc3426e5c
krypek <115574014+krypciak@users.noreply.github.com>
2022-12-31 17:40:49 +0100

    Update start_rss.sh

--- scripts/auto-download/start_rss.sh
+++ scripts/auto-download/start_rss.sh
@@ -54,7 +54,7 @@ function listen_rss() {
 i=0
 for feed in ${CHANNEL_FEEDS[@]}; do
     listen_rss "${INSTANCES[$i]}/feed/channel/$feed" &
-
+    sleep 5
     i=$((i+1))
     if [[ ${#INSTANCES[@]} -eq $i ]]; then
         i=0
@@ -63,5 +63,6 @@ done
 
 for feed in ${ODYSEE_FEEDS[@]}; do
     listen_rss "https://odysee.com/$/rss/$feed" &
+    sleep 5
 done
 
