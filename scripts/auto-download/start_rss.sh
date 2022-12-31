3ac9b0764079596306a8fd8e08b8578ab96e0562
krypek <115574014+krypciak@users.noreply.github.com>
2022-12-31 22:47:47 +0100

    Update start_rss.sh

--- scripts/auto-download/start_rss.sh
+++ scripts/auto-download/start_rss.sh
@@ -20,9 +20,7 @@ YT_DLP_ARGS='--sponsorblock-remove all --embed-thumbnail --add-metadata --embed-
 
 TITLE='%(channel)s - %(title)s.%(ext)s'
 
-
-function listen_rss() {
-    rsstail -z -l -N -n 1 -i 300 -P -u "$1" | while read url; do
+function check_space() {
         # If there is less than 2 GiB space left
         while [[ "$(df $VIDEOS_DIR --output=avail | tail +2)" -le 2097152 ]]; do
             # If no files left to delete, exit
@@ -34,6 +32,12 @@ function listen_rss() {
             last_file="$(LC_CTYPE=C ls -t $VIDEOS_DIR | tail -1)"
             rm -f "$last_file"
         done
+}
+
+
+function listen_rss() {
+    rsstail -z -l -N -n 1 -i 300 -P -u "$1" | while read url; do
+        check_space
 
         if [[ "$url" == http* ]]; then 
             echo -e "${BLUE}Recived RSS URL: ${GREEN}${url}${NC}"
@@ -46,6 +50,7 @@ function listen_rss() {
                 echo -e "${GREEN}Downloading URL: ${BLUE}${url}${GREEN}  Filename: '${BLUE}${filename}${GREEN}'${NC}"
                 yt-dlp $YT_DLP_ARGS -o "$TITLE" -P "$VIDEOS_DIR" -P "temp:$TEMP_DIR" "$url"
                 echo -e "${GREEN}Video: '${BLUE}${filename}${GREEN}' download done${NC}"
+                check_space
             fi
         fi
     done
