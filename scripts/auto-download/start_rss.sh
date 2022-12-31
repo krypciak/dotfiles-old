d896563682d02722264ea9631626b32c92bbcada
krypek <115574014+krypciak@users.noreply.github.com>
2022-12-31 15:27:18 +0100

    Update start_rss.sh

--- scripts/auto-download/start_rss.sh
+++ scripts/auto-download/start_rss.sh
@@ -22,26 +22,27 @@ TITLE='%(channel)s - %(title)s.%(ext)s'
 
 function listen_rss() {
     rsstail -z -l -N -n 1 -i 300 -P -u "$1" | while read url; do
-        echo -e "${BLUE}Recived RSS URL: ${GREEN}${url}${NC}"
-
         # If there is less than 2 GiB space left
-        while [[ "$(df $VIDEOS_DIR | awk 'NR==2{print $4}')" -le 2097152 ]]; do
+        while [[ "$(df $VIDEOS_DIR --output=avail | tail +2)" -le 2097152 ]]; do
             # If no files left to delete, exit
             if [[ "$(ls $VIDEOS_DIR | wc -l)" -eq 0 ]]; then
                 echo -e "${RED}Less than 2GiB on $VIDEOS_DIR, exiting${NC}"
                 exit 1
             fi
             # Remove the last video
-            rm -f "$(LC_CTYPE=C ls -t $VIDEOS_DIR | tail -1)"
+            last_file="$(LC_CTYPE=C ls -t $VIDEOS_DIR | tail -1)"
+            rm -f "$last_file"
         done
 
         if [[ "$url" == http* ]]; then 
-            filename=$(yt-dlp "$url" -o "$TITLE" --print filename)
-            old_filename=$(yt-dlp "$url" -o "$TITLE" --restrict-filenames --print filename)
+            echo -e "${BLUE}Recived RSS URL: ${GREEN}${url}${NC}"
+
+            filename=$(yt-dlp "$url" -o "$TITLE" --no-warnings --print filename)
+            old_filename=$(yt-dlp "$url" -o "$TITLE" --no-warnings --restrict-filenames --print filename)
             if [ -f "$VIDEOS_DIR/$filename" ] || [ -f "$VIDEOS_DIR/$old_filename" ]; then 
-                echo -e "${BLUE}File exists, skipping${NC}"
+                echo -e "${GREEN}Filename: '${BLUE}${filename}${GREEN}' exists, skipping${NC}"
             else
-                echo -e "${BLUE}Downloading URL: ${GREEN}${url}${BLUE}  Filename: ${GREEN}${filename}${NC}"
+                echo -e "${GREEN}Downloading URL: ${BLUE}${url}${GREEN}  Filename: '${BLUE}${filename}${GREEN}'${NC}"
                 yt-dlp $YT_DLP_ARGS -o "$TITLE" -P "$VIDEOS_DIR" -P "temp:$TEMP_DIR" "$url"
                 echo -e "${GREEN}Video: '${BLUE}${filename}${GREEN}' download done${NC}"
             fi
@@ -52,7 +53,7 @@ function listen_rss() {
 i=0
 for feed in ${CHANNEL_FEEDS[@]}; do
     listen_rss "${INSTANCES[$i]}/feed/channel/$feed" &
-    sleep 300
+    #sleep 300
 
     i=$((i+1))
     if [[ ${#INSTANCES[@]} -eq $i ]]; then
@@ -62,6 +63,6 @@ done
 
 for feed in ${ODYSEE_FEEDS[@]}; do
     listen_rss "https://odysee.com/$/rss/$feed" &
-    sleep 300
+    #sleep 300
 done
 
