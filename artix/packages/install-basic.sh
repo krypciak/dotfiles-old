7afa32a82214323db501ac02925c02626ac41b5a
krypek <115574014+krypciak@users.noreply.github.com>
2023-01-24 14:46:35 +0100

    Add 'xorg-server-xvfb'

--- artix/packages/install-basic.sh
+++ artix/packages/install-basic.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 function install_basic() {
-    echo 'dbus dbus-openrc dbus-python dbus-glib fzf perl python rmtrash trash-cli ttf-dejavu ttf-hack python-pip htop autojump git neofetch neovim-symlinks ranger syntax-highlighting tldr util-linux wget lsd dos2unix p7zip unrar unzip zip mandoc greetd-artix-openrc greetd-tuigreet-bin opendoas-sudo nvim-packer-git net-tools fish opendoas efibootmgr grub dosfstools mtools tmux galaxy/ttf-nerd-fonts-symbols-2048-em util-linux atuin bat bottom dog dust fd hyperfine lfs procs tokei imagemagick ntp-openrc lua-language-server pyright rust-analyzer moreutils bc'
+    echo 'dbus dbus-openrc dbus-python dbus-glib fzf perl python rmtrash trash-cli ttf-dejavu ttf-hack python-pip htop autojump git neofetch neovim-symlinks ranger syntax-highlighting tldr util-linux wget lsd dos2unix p7zip unrar unzip zip mandoc greetd-artix-openrc greetd-tuigreet-bin opendoas-sudo nvim-packer-git net-tools fish opendoas efibootmgr grub dosfstools mtools tmux galaxy/ttf-nerd-fonts-symbols-2048-em util-linux atuin bat bottom dog dust fd hyperfine lfs procs tokei imagemagick ntp-openrc lua-language-server pyright rust-analyzer moreutils bc xorg-server-xvfb'
 }
 
 function configure_basic() {
