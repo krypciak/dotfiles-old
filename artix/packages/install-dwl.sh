f0adb2c6377801ba06475e3e89b24eb269aaf2f1
krypek <115574014+krypciak@users.noreply.github.com>
2023-01-24 21:52:24 +0100

    Parallelize dwl compilation

--- artix/packages/install-dwl.sh
+++ artix/packages/install-dwl.sh
@@ -5,16 +5,17 @@ function install_dwl() {
 
 function configure_dwl() {
     cd $USER_HOME/.config/dwl/dwl-dotfiles/
-    doas -u $USER1 make
+    doas -u $USER1 make &
     
     cd $USER_HOME/.config/dwl/somebar/
-    doas -u $USER1 meson build
+    doas -u $USER1 meson build &
+    
     cd $USER_HOME/.config/dwl/somebar/build
-    doas -u $USER1 ninja
-
-    cd $USER_HOME/.config/dwl/dpms-off
-    doas -u $USER1 cargo build --release
+    doas -u $USER1 ninja &
 
     cd $USER_HOME/.config/dwl/someblocks
-    doas -u $USER1 make
+    doas -u $USER1 make &
+
+    cd $USER_HOME/.config/dwl/dpms-off
+    doas -u $USER1 cargo build --release &
 }
