ff7812ab46954345675b44cbfe91bc8d12f9f2da
krypek <115574014+krypciak@users.noreply.github.com>
2023-02-20 12:12:23 +0100

    iforgothowtosyncfork v2

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -51,5 +51,5 @@ if status is-interactive
         end
     end
 
-    alias iforgothowtosyncfork='printf "# Don\'t worry.\n# Sync your fork\ngit fetch upstream\ngit checkout master\ngit merge upstream/master\n"'
+    alias iforgothowtosyncfork='printf "# Don\'t worry.\n# Sync your fork\ngit fetch upstream\ngit checkout yamainbrammch\ngit merge upstream\n"'
 end
