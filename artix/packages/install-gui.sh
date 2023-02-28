6cfcf5454b92320a4a9073f00bc6e40a91e7d7e7
krypek <115574014+krypciak@users.noreply.github.com>
2023-02-28 14:21:49 +0100

    Update broken packages

--- artix/packages/install-gui.sh
+++ artix/packages/install-gui.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function install_gui() {
-    echo 'alacritty breeze breeze-gtk breeze-icons feh lxappearance qt5-base qt6-base xdg-desktop-portal zenity topgrade xdg-utils gtk2 gtk3 qt5ct kolourpaint mpv tutanota-desktop-bin world/gnome-keyring aerc pandoc-bin'
+    echo 'alacritty breeze breeze-gtk breeze-icons feh lxappearance qt5-base qt6-base xdg-desktop-portal zenity xdg-utils gtk2 gtk3 qt5ct kolourpaint mpv tutanota-desktop-bin world/gnome-keyring aerc pandoc-bin'
     if [ $INSTALL_CRON -eq 1 ]; then
         echo ' cronie-openrc'
     fi
