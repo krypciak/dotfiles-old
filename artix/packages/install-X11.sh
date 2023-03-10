aac824ed83c3c25915c5801268416c9a5c1c591a
krypek <115574014+krypciak@users.noreply.github.com>
2023-03-10 20:26:34 +0100

    Add 'xclip'

--- artix/packages/install-X11.sh
+++ artix/packages/install-X11.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 function install_X11() {
-    echo 'unclutter-xfixes-git xbindkeys xdotool xkeyboard-config xorg-server xorg-xinit xorg-xprop xorg-xrandr xorg-xmodmap redshift scrot xsel xorg-xeyes rofi clipmenu alock' 
+    echo 'unclutter-xfixes-git xbindkeys xdotool xkeyboard-config xorg-server xorg-xinit xorg-xprop xorg-xrandr xorg-xmodmap redshift scrot xsel xorg-xeyes rofi clipmenu alock xclip' 
 }
 
