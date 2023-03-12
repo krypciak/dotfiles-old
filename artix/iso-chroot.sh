89d35dca2da4afd7cacae5b8cd1ad17705c4c288
krypek <115574014+krypciak@users.noreply.github.com>
2023-03-12 21:57:42 +0100

    Update iso-chroot.sh

--- artix/iso-chroot.sh
+++ artix/iso-chroot.sh
@@ -17,7 +17,7 @@ pri "Copying the repo to $DOTFILES_DIR"
 mkdir -p $DOTFILES_DIR/..
 cp -rf $ARTIXD_DIR/../ $DOTFILES_DIR/
 
-printf "3\n2\n" > $DOTFILES_DIR/dotfiles/wallpapers/selected
+printf "3\n1\n" > $DOTFILES_DIR/dotfiles/wallpapers/selected
 
 
 export ARTIXD_DIR=$DOTFILES_DIR/artix
