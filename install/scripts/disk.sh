08a35c938a9bd61b9a634cf6394b20e4d9915600
krypek <115574014+krypciak@users.noreply.github.com>
2023-04-16 00:40:05 +0200

    Update disk.sh

--- install/scripts/disk.sh
+++ install/scripts/disk.sh
@@ -156,7 +156,7 @@ fi
 
 source $SCRIPTS_DIR/live/$VARIANT/strap-packages.sh
 
-_USER_CONFIG_DIR="$INSTALL_DIR$FAKE_USER_HOME/.config/"
+_USER_CONFIG_DIR="$INSTALL_DIR$FAKE_USER_HOME/.config"
 mkdir -p $_USER_CONFIG_DIR
 info "Copying the repo to $_USER_CONFIG_DIR/dotfiles"
 cp -rf $DOTFILES_DIR/ $_USER_CONFIG_DIR/
