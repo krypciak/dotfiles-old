5be61ddedf65c139d4030bcf65951ce3828d1fb1
krypek <115574014+krypciak@users.noreply.github.com>
2023-04-15 19:22:58 +0200

    Own user home even if user exists

--- install/scripts/live/add-user.sh
+++ install/scripts/live/add-user.sh
@@ -4,7 +4,7 @@ info "Adding user $USER1"
 if ! id "$USER1" &>/dev/null; then
     useradd -s /bin/bash -G tty,ftp,games,network,scanner,users,video,audio,wheel $USER1
     mkdir -p $USER_HOME
-    chown -R $USER1:$USER_GROUP $USER_HOME/
-    chown -R $USER1:$USER_GROUP $DOTFILES_DIR
 fi
 
+chown -R $USER1:$USER_GROUP $USER_HOME/
+chown -R $USER1:$USER_GROUP $DOTFILES_DIR
