8731a7ee9951003fba6d2923154d6cb01516d14d
krypek <115574014+krypciak@users.noreply.github.com>
2023-04-15 19:38:20 +0200

    Fix package installing

--- install/scripts/live/temp-doas.sh
+++ install/scripts/live/temp-doas.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 info "Creating temporary doas config"
-echo "permit nopass setenv { YOLO USER1 PACMAN_ARGUMENTS PARU_ARGUMENTS } root" > /etc/doas.conf
-echo "permit nopass setenv { YOLO USER1 PACMAN_ARGUMENTS PARU_ARGUMENTS } $USER1" >> /etc/doas.conf
+echo "permit nopass setenv { YOLO USER1 PACMAN_ARGUMENTS PARU_ARGUMENTS PACKAGE_LIST } root" > /etc/doas.conf
+echo "permit nopass setenv { YOLO USER1 PACMAN_ARGUMENTS PARU_ARGUMENTS PACKAGE_LIST } $USER1" >> /etc/doas.conf
 
