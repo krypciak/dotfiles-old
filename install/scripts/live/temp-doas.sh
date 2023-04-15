ccdfe950234d8c4f30bd763ab5e9998734c6dc49
krypek <115574014+krypciak@users.noreply.github.com>
2023-04-15 20:07:48 +0200

    Export USER_GROUP

--- install/scripts/live/temp-doas.sh
+++ install/scripts/live/temp-doas.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 info "Creating temporary doas config"
-echo "permit nopass setenv { YOLO USER1 PACMAN_ARGUMENTS PARU_ARGUMENTS PACKAGE_LIST } root" > /etc/doas.conf
-echo "permit nopass setenv { YOLO USER1 PACMAN_ARGUMENTS PARU_ARGUMENTS PACKAGE_LIST } $USER1" >> /etc/doas.conf
+echo "permit nopass setenv { YOLO USER1 USER_GROUP PACMAN_ARGUMENTS PARU_ARGUMENTS PACKAGE_LIST } root" > /etc/doas.conf
+echo "permit nopass setenv { YOLO USER1 USER_GROUP PACMAN_ARGUMENTS PARU_ARGUMENTS PACKAGE_LIST } $USER1" >> /etc/doas.conf
 
