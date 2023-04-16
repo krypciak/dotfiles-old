31e1c0ff2dbbdce28d5d178cecb581918ca44877
krypek <115574014+krypciak@users.noreply.github.com>
2023-04-16 16:19:26 +0200

    Update configure-packages.sh

--- install/scripts/live/configure-packages.sh
+++ install/scripts/live/configure-packages.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 for group in "${PACKAGE_GROUPS[@]}"; do
+    source $PACKAGES_DIR/${group}.sh
     CONFIG_FUNC="${VARIANT}_${group}_configure"
     if command -v "$CONFIG_FUNC" &> /dev/null; then
         info "Configuring $group"
