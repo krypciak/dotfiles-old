03fddeb892d95c7e761991e1773a4b5ef8c99cbf
krypek <115574014+krypciak@users.noreply.github.com>
2023-04-16 17:56:57 +0200

    Install nvim packer properly

--- install-dotfiles-root.sh
+++ install-dotfiles-root.sh
@@ -17,7 +17,7 @@ chown -R root:root /root/.config/at_login.sh
 
 # Install nvim packer
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim > /dev/null 2>&1
-# Update nvim plugins
-echo Updating neovim plugins...
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' > /dev/null 2>&1
-echo Done.
+## Update nvim plugins
+#echo Updating neovim plugins...
+#nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' > /dev/null 2>&1
+#echo Done.
