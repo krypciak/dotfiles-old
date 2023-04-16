3f34ad4afd3a8e8c45787bccac62a0b0a4c73aaa
krypek <115574014+krypciak@users.noreply.github.com>
2023-04-16 14:29:53 +0200

    Remove some useless drivers

--- install/packages/drivers.sh
+++ install/packages/drivers.sh
@@ -13,12 +13,12 @@ _artix_arch_driver_install() {
             cp $COMMON_CONFIGS_DIR/20-amdgpu.conf /etc/X11/xorg.conf.d/
         elif [ "$GPU" == 'ati' ]; then DRIVER_LIST="$DRIVER_LIST xf86-video-ati amdvlk lib32-amdvlk vulkan-radeon lib32-vulkan-radeon"
         elif [ "$GPU" == 'intel' ]; then DRIVER_LIST="$DRIVER_LIST xf86-video-intel vulkan-intel lib32-vulkan-intel"
-        elif [ "$GPU" == 'nvidia' ]; then DRIVER_LIST="$DRIVER_LIST xf86-video-nouveau nvidia-utils"
+        elif [ "$GPU" == 'nvidia' ]; then DRIVER_LIST="$DRIVER_LIST xf86-video-nouveau"
         else confirm "Invalid GPU: $GPU" "ignore"; fi
     
     elif [ $ALL_DRIVERS -eq 1 ]; then
         DRIVER_LIST="$DRIVER_LIST amd-ucode intel-ucode"
-        DRIVER_LIST="$DRIVER_LIST xf86-input-vmmouse xf86-video-amdgpu xf86-video-ati xf86-video-dummy xf86-video-fbdev xf86-video-intel xf86-video-nouveau xf86-video-openchrome xf86-video-sisusb xf86-video-vesa xf86-video-vmware xf86-video-voodoo"
+        DRIVER_LIST="$DRIVER_LIST xf86-input-vmmouse xf86-video-amdgpu xf86-video-ati xf86-video-dummy xf86-video-fbdev xf86-video-intel xf86-video-nouveau xf86-video-vesa xf86-video-vmware"
     fi
     echo $DRIVER_LIST
 }
