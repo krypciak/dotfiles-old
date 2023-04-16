3641a403dca45911059bf7e5400a379704728f71
krypek <115574014+krypciak@users.noreply.github.com>
2023-04-16 14:52:47 +0200

    Bug fix

--- install/scripts/live/artix/chroot-into.sh
+++ install/scripts/live/artix/chroot-into.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 confirm "Chroot into ${INSTALL_DIR}?"
-artix-chroot $INSTALL_DIR sh -c "export $(env | grep -E 'CONF_FILES_DIR|SCRIPTS_DIR|MODE|VARIANT|VARIANT_NAME|TYPE' | xargs); $USER_HOME/home/.config/dotfiles/install/scripts/live/live.sh"
+artix-chroot $INSTALL_DIR sh -c "export $(env | grep -E '^(CONF_FILES_DIR|SCRIPTS_DIR|MODE|VARIANT|VARIANT_NAME|TYPE)' | xargs); $USER_HOME/home/.config/dotfiles/install/scripts/live/live.sh"
