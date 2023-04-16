dfe9d79cdc3cf6b126e937cadc3d3c82d3a7c9f3
krypek <115574014+krypciak@users.noreply.github.com>
2023-04-16 18:03:28 +0200

    Fix fstab info not filled

--- install/scripts/live/configure-fstab.sh
+++ install/scripts/live/configure-fstab.sh
@@ -5,6 +5,7 @@ sed -i "s|ROOT_UUID|$(blkid $LVM_DIR/root -s UUID -o value)|g" /etc/fstab
 [ "$ENABLE_SWAP" == '1' ] && sed -i "s|SWAP_UUID|$(blkid $LVM_DIR/swap -s UUID -o value)|g" /etc/fstab
 sed -i "s|HOME_UUID|$(blkid $LVM_DIR/home -s UUID -o value)|g" /etc/fstab
 sed -i "s|BOOT_UUID|$(blkid $BOOT_PART -s UUID -o value)|g" /etc/fstab
+sed -i "s|BOOT_PART|$(BOOT_PART)|g" /etc/fstab
 sed -i "s|LVM_GROUP_NAME|$LVM_GROUP_NAME|g" /etc/fstab
 sed -i "s|ROOT_FSTAB_ARGS|$ROOT_FSTAB_ARGS|g" /etc/fstab
 sed -i "s|HOME_FSTAB_ARGS|$HOME_FSTAB_ARGS|g" /etc/fstab
