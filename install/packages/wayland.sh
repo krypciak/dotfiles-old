749292b4eb2ec83e81cc1b826070478eedf98a9b
krypek <krypek@tuta.io>
2023-04-17 21:49:38 +0200

    Move gamescope to gaming.sh

--- install/packages/wayland.sh
+++ install/packages/wayland.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 artix_wayland_install() {
-    echo 'cage cliphist fuzzel gamescope gammastep grim inotify-tools qt6-wayland slurp swaylock swww wayland wayland-protocols wlroots wlr-randr xorg-xwayland'
+    echo 'cage cliphist fuzzel gammastep grim inotify-tools qt6-wayland slurp swaylock swww wayland wayland-protocols wlroots wlr-randr xorg-xwayland'
 }
 
 arch_wayland_install() {
-    echo 'cliphist fuzzel gamescope gammastep grim inotify-tools qt6-wayland slurp swaylock swww wayland wayland-protocols wlroots wlr-randr xorg-xwayland'
+    echo 'cliphist fuzzel gammastep grim inotify-tools qt6-wayland slurp swaylock swww wayland wayland-protocols wlroots wlr-randr xorg-xwayland'
 }
 
