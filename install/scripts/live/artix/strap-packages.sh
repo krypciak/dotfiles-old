c3f2be553b428fdca222cbcba9290c5fba9ec2c6
krypek <krypek@tuta.io>
2023-04-18 18:17:50 +0200

    Query strap packages from base package group

--- install/scripts/live/artix/strap-packages.sh
+++ install/scripts/live/artix/strap-packages.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 confirm "Install basic packages to ${INSTALL_DIR}?"
-basestrap -C $COMMON_CONFIGS_DIR/pacman.conf.install $INSTALL_DIR base openrc elogind-openrc artix-keyring artix-mirrorlist pacman autoconf automake bison fakeroot flex gcc groff libtool m4 make patch pkgconf texinfo which iptables-nft $KERNEL $KERNEL-headers util-linux openntpd-openrc > $OUTPUT
+source $PACKAGES_DIR/base.sh
+basestrap -C $COMMON_CONFIGS_DIR/pacman.conf.install $INSTALL_DIR $(${VARIANT}_base_install)
