09f1e6f05b519a8794711404347cf905ace57984
krypek <krypek@tuta.io>
2023-04-22 18:01:57 +0200

    Add shellcheck

--- install/packages/coding.sh
+++ install/packages/coding.sh
@@ -2,10 +2,10 @@
 
 # github-desktop-bin
 artix_install_coding() {
-    echo 'eclipse-java git-filter-repo jdk17-openjdk jdk8-openjdk jdk-openjdk jre-openjdk jre-openjdk-headless rust'
+    echo 'eclipse-java git-filter-repo jdk17-openjdk jdk8-openjdk jdk-openjdk jre-openjdk jre-openjdk-headless rust shellcheck'
 }
 
 arch_install_coding() {
-    echo 'eclipse-java git-filter-repo jdk17-openjdk jdk8-openjdk jdk-openjdk jre-openjdk jre-openjdk-headless rust'
+    echo 'eclipse-java git-filter-repo jdk17-openjdk jdk8-openjdk jdk-openjdk jre-openjdk jre-openjdk-headless rust shellcheck'
 }
 
