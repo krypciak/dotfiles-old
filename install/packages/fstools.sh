ef1106d4da277131c079ba1585e1d4f6928e855f
krypek <krypek@tuta.io>
2023-04-26 19:45:04 +0200

    Add 'cdrtools'

--- install/packages/fstools.sh
+++ install/packages/fstools.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 artix_fstools_install() {
-    echo 'artools-base btrfs-progs dos2unix dosfstools e2fsprogs exfat-utils f2fs-tools nilfs-utils ntfs-3g sysfsutils testdisk udftools ventoy-bin'
+    echo 'artools-base btrfs-progs cdrtools dos2unix dosfstools e2fsprogs exfat-utils f2fs-tools nilfs-utils ntfs-3g sysfsutils testdisk udftools ventoy-bin'
 }
 
 arch_fstools_install() {
-    echo 'btrfs-progs dos2unix dosfstools e2fsprogs exfat-utils f2fs-tools nilfs-utils ntfs-3g sysfsutils testdisk udftools ventoy-bin'
+    echo 'btrfs-progs cdrtools dos2unix dosfstools e2fsprogs exfat-utils f2fs-tools nilfs-utils ntfs-3g sysfsutils testdisk udftools ventoy-bin'
 }
 
