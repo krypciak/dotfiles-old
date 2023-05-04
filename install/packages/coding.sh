7c2255a33b772596196c4bfc910fae88ba146ae8
krypek <krypek@tuta.io>
2023-05-04 22:14:53 +0200

    Add github-cli

--- install/packages/coding.sh
+++ install/packages/coding.sh
@@ -2,11 +2,11 @@
 
 # github-desktop-bin
 artix_coding_install() {
-    echo 'eclipse-java git-filter-repo jdk17-openjdk jdk8-openjdk jdk-openjdk jre-openjdk jre-openjdk-headless rust shellcheck'
+    echo 'eclipse-java git-filter-repo github-cli jdk17-openjdk jdk8-openjdk jdk-openjdk jre-openjdk jre-openjdk-headless rust shellcheck'
 }
 
 arch_coding_install() {
-    echo 'eclipse-java git-filter-repo jdk17-openjdk jdk8-openjdk jdk-openjdk jre-openjdk jre-openjdk-headless rust shellcheck'
+    echo 'eclipse-java git-filter-repo github-cli jdk17-openjdk jdk8-openjdk jdk-openjdk jre-openjdk jre-openjdk-headless rust shellcheck'
 }
 
 artix_coding_configure() {
