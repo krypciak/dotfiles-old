95ce1fdc974c86377aad4aedae85c3450152fd50
krypek <krypek@tuta.io>
2023-05-04 20:50:15 +0200

    Fix coloring issue

--- dotfiles/.config/fish/functions/fish_right_prompt.fish
+++ dotfiles/.config/fish/functions/fish_right_prompt.fish
@@ -2,6 +2,7 @@ function fish_right_prompt
     set -l cmd_status $status
     if test $cmd_status -ne 0
         echo -n (set_color red)"✘ $cmd_status"
+        set_color normal
     end
 
     return
