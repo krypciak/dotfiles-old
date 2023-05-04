115089a7a4c5436658f3ab43ee6ad5b1b04f9f09
krypek <krypek@tuta.io>
2023-05-04 15:03:49 +0200

    Add more info

--- install/scripts/live/artix/iso.sh
+++ install/scripts/live/artix/iso.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
 if [ "$NET" = 'offline' ]; then
+    info "Reinstalling kernel"
     PACKAGES="$KERNEL"
     PP="$PACKAGES_DIR/offline/$VARIANT/packages"
     for pack in $PACKAGES; do
-        pacman --noconfirm -U $PP/$pack-*.pkg.tar.zst > /dev/null 2>&1
+        pacman --noconfirm -U $PP/$pack-*.pkg.tar.zst > $OUTPUT 2>&1
     done
     # artix-grub-live artix-grub-theme
 fi
 #pacman --noconfirm -S mkinitcpio-nfs-utils squashfs-tools artix-live-openrc iso-initcpio 
 
 if [ ! -f /boot/amd-ucode.img ] || [ ! -f /boot/intel-ucode.img ]; then
+    info "Reinstalling ucode's"
     pacman --noconfirm -S amd-ucode intel-ucode > $OUTPUT 2>&1
 fi
 
